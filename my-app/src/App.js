e70ef0762df0207cc2c070c48836d98fb9ecf3d3
Abhinav Arora <abhinavaroraa14799@gmail.com>
2022-03-05 11:07:42 -0800

    added the backedn

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello guys, Wlecome to our 462 Project !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        let result = await fetch(
+        'http://localhost:5000/register', {
+            method: "post",
+            body: JSON.stringify({ name, email }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        result = await result.json();
+        console.warn(result);
+        if (result) {
+            alert("Data saved successfully");
+            setEmail("");
+            setName("");
+        }
+    }
+    return (
+        <>
+            <h1>This is React WebApp </h1>
+            
+Welcome guys to our 462 Project ! Backend is setup and working fine.
+        </>
+    );
 }
 
 export default App;
